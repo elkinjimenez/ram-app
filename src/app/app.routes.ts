0c0c43de451d468055bd73304865fc2f9f09a246
elkinjimenez <elkin.jiga@hotmail.com>
2025-03-13 16:43:20 -0500

    Para desplegar en Web Parte 5.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadComponent: () => import('./pages/intro/intro.page').then(m => m.IntroPage),
+  //   pathMatch: 'full',
+  // },
   {
     path: '',
-    loadComponent: () => import('./pages/intro/intro.page').then(m => m.IntroPage),
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage),
   },
   {
